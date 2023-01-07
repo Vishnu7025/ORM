41f23bfe854acb49688bd3fd8d0780c9f34637c7
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2023-01-07 10:54:21 +0530

    admin side display

--- modelDemo/empApp/admin.py
+++ modelDemo/empApp/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import Employee
 
 # Register your models here.
-admin.site.register(Employee)
+class emplyeAdmin(admin.ModelAdmin):
+    list_display=['firstName','salary','email']
+admin.site.register(Employee,emplyeAdmin)
