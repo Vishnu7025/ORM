bce6b65b20034d22ffd6ab86f60d29a085111ed9
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2023-01-07 10:55:50 +0530

    d

--- modelDemo/empApp/admin.py
+++ modelDemo/empApp/admin.py
@@ -3,5 +3,5 @@ from .models import Employee
 
 # Register your models here.
 class emplyeAdmin(admin.ModelAdmin):
-    list_display=['firstName','salary','email']
+    list_display=['firstName','lastName','salary','email']
 admin.site.register(Employee,emplyeAdmin)
