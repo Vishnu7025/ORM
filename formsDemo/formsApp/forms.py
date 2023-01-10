b079857ff284ad90add706fc64c168a1fb454c77
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2023-01-10 19:24:46 +0530

    n

--- formsDemo/formsApp/forms.py
+++ formsDemo/formsApp/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
 class UserRegistrationForm(forms.Form):
+    GENDER=[('male','MALE'),('female','FEMALE')]
     firstName = forms.CharField()
     lastName = forms.CharField()
-    email = forms.CharField()
+    email = forms.CharField(widget=forms.Select(choices=GENDER))
+    password = forms.CharField(widget=forms.PasswordInput)
--- formsDemo/formsApp/views.py
+++ formsDemo/formsApp/views.py
@@ -3,4 +3,12 @@ from formsApp import forms
 # Create your views here.
 def userRegistrationView(request):
     form = forms.UserRegistrationForm()
+    if request.method=='POST':
+        form = forms.UserRegistrationForm(request.POST)
+        if form.is_valid():
+            print("form is valid")
+            print("FirstName",form.cleaned_data['firstName'])
+            print("LastName",form.cleaned_data['lastName'])
+            print("Email",form.cleaned_data['email'])
+
     return render(request,'userRegistration.html',{'form':form})
