2cd73a29406004009103520fc47050ffd9e7ef42
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2023-01-13 18:21:57 +0530

    CRUD OPERATION USING CLASS BASED VIEWS

--- classBasedViews/cbvApp/views.py
+++ classBasedViews/cbvApp/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.views.generic  import View
 from django.http import HttpResponse
 # Create your views here.
-class GreetingView(View):
-    def get(self,request):
-        return HttpResponse("first sbv says")
+
+
+
+
+
+# class GreetingView(View):
+#     def get(self,request):
+#         return HttpResponse("first sbv says")
