ad83b2f544441bac83c4b856485ba1ae44fd8409
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2023-01-13 19:34:20 +0530

    detail view

--- classBasedViews/cbvApp/views.py
+++ classBasedViews/cbvApp/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.views.generic  import View
-from django.views.generic  import ListView
+from django.views.generic  import ListView,DetailView
 from django.http import HttpResponse
 from .models import Student
 # Create your views here.
 
 class StudentListView(ListView):
     model = Student
-    
+class StudentDetails(DetailView):
+    model = Student
+
 
 
 
--- classBasedViews/classBasedViews/urls.py
+++ classBasedViews/classBasedViews/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from cbvApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.StudentListView.as_view())
+    path('',views.StudentListView.as_view()),
+    path('<int:pk>/',views.StudentDetails.as_view())
     # path('',views.GreetingView.as_view())
 ]
