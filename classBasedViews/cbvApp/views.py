fd5305a20de6df7799ca208d9cc9297ee0da1c98
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2023-01-13 17:54:40 +0530

    views

--- classBasedViews/cbvApp/views.py
+++ classBasedViews/cbvApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.views.generic  import View
+from django.http import HttpResponse
 # Create your views here.
+class GreetingView(View):
+    def get(self,request):
+        return HttpResponse("first sbv says")
