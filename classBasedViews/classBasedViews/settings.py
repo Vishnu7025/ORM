6c57621d45a124fb6feff3c9941b3daeba1b8a3b
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2023-01-13 18:56:39 +0530

    eroor

--- classBasedViews/classBasedViews/settings.py
+++ classBasedViews/classBasedViews/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'classBasedViews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
