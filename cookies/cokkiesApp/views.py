f5bb9c4b0e80e77800197775e909ff654cfcb22e
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2023-01-17 17:59:07 +0530

    view

--- cookies/cokkiesApp/views.py
+++ cookies/cokkiesApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+    request.session.set_test_cookie()
+    return HttpResponse("<h1>home page </h1>")
+
+def page2(request):
+    if request.session.test_cookie_worked():
+        print('cookies are enabled')
+    request.session.delete_test_cookie()
+    return HttpResponse("<b> page2 </b>")
