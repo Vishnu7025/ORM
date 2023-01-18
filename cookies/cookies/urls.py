ba9d25fee3317d2f3fcccd9c24c4d5f5beb29798
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2023-01-18 19:31:38 +0530

    views

--- cookies/cookies/urls.py
+++ cookies/cookies/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cokkiesApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/',views.home),
+    path('page2/',views.page2)
 ]
